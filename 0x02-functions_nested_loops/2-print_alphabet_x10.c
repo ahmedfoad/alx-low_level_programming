1c1126cf051f726c004d64bdf55f6e76f599654b
ahmedfoad <eng.ahmedfoad@gmail.com>
2023-03-22 01:13:57 +0200

    2. 10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints all characters from a to z.
+ */
+void print_alphabet_x10(void)
+{
+	int x;
+
+	for (x = 0; x < 10; x++)
+	{
+		print_alphabet();
+	}
+}
