7e7233da558e06812e4c0cbdbdfc4f133e9674f4
ahmedfoad <eng.ahmedfoad@gmail.com>
2023-03-23 00:29:39 +0200

    Update - remove the return 0 from function positive_or_negative

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -6,7 +6,7 @@
  * positive_or_negative - Prints an intege number and states
  * whether it is positive, negative, or zero.
  * @i: The number to be checked
- * Return: Always 0.
+ * Return: Always viod or no return.
 */
 void positive_or_negative(int i)
 {
@@ -16,7 +16,5 @@ void positive_or_negative(int i)
 		printf("%d is negative\n", i);
 	else
 		printf("%d is zero\n", i);
-
-	return (0);
 }
 
