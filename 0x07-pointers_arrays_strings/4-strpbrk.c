af3e028f21c7a5ccfe26539a073f19fe463829cd
ahmedfoad <eng.ahmedfoad@gmail.com>
2023-04-04 00:57:52 +0200

    betty the code

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -8,7 +8,8 @@
  * @accept: substring
  *
  *
- * Return: Returns a pointer to the byte in s that matches one of the bytes in accept, or NULL if no such byte is found.
+ * Return: Returns a pointer to the byte in s that matches one of the bytes
+ * in accept, or NULL if no such byte is found.
  */
 char *_strpbrk(char *s, char *accept)
 {
