9e6b0ec69e349db2b393c0de5bb218321704ee18
ahmedfoad <eng.ahmedfoad@gmail.com>
2023-04-04 23:04:58 +0200

    8. Double pointer, double fun

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * set_string - replace with the value for array to
+ *
+ * @s: arrary of s that we need to replace
+ * @to: array of char that we will take
+ * Return: void
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
