75691985bdb7f9b6d2d01df6dca3e037d43b49a3
ahmedfoad <eng.ahmedfoad@gmail.com>
2023-04-18 01:42:44 +0200

    Fix Free memory

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -14,12 +14,35 @@
 */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *my_dog = malloc(sizeof(dog_t));
+	unsigned int nl, ol;
+
+	dog_t *my_dog;
+
+	if (name == NULL || owner == NULL)
+		return (NULL);
+
+	my_dog = malloc(sizeof(dog_t));
 
 	if (my_dog != NULL)
 	{
+		for (nl = 0; name[nl]; nl++)
+			nl++;
+		(*my_dog).name = malloc(nl * sizeof(char));
+		if (my_dog->name == NULL)
+		{
+			free(my_dog);
+			return (NULL);
+		}
 		(*my_dog).name = name;
 		(*my_dog).age = age;
+		for (ol = 0; owner[ol]; ol++)
+			ol++;
+		(*my_dog).owner =  malloc(ol * sizeof(char));
+		if (my_dog->owner == NULL)
+		{
+			free(my_dog);
+			return (NULL);
+		}
 		(*my_dog).owner = owner;
 	}
 	else
