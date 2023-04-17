603a4c9b4f76ba4e735b53d4aea0bd7cb858e2b4
ahmedfoad <eng.ahmedfoad@gmail.com>
2023-04-18 00:29:07 +0200

    0. Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+* struct dog - Struct that display the dog's name, age, and the owner name.
+*
+* @name: Displays the dog's name.
+*
+* @age: Displays the dog's age.
+*
+* @owner: Displays the dog's owner name.
+*/
+
+struct dog
+{
+	char *name; /* 8 bytes */
+	float age; /* 4 bytes */
+	char *owner; /* 8 bytes */
+};
+
+#endif
