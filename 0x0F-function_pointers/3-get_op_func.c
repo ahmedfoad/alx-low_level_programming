6d2f3db1833be95d22746334ca145d3cce7331b4
AhmedFAly <eng.ahmedfoad@gmail.com>
2023-04-26 19:49:22 +0200

    fix betty

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -3,6 +3,13 @@
 #include <string.h>
 #include <stdio.h>
 
+/**
+  * get_op_func - function that selects the correct
+  * function to perform the operation asked by the user
+  * @s: operator
+  *
+  * Return: integer of the math equation
+  */
 int (*get_op_func(char *s))(int, int)
 {
 	op_t ops[] = {
