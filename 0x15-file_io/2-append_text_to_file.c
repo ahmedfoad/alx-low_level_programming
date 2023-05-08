53ee3d47d12de834c051cf6a8080ec6475b2d430
AhmedFAly <eng.ahmedfoad@gmail.com>
2023-05-08 22:52:22 +0300

    betty

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -14,15 +14,15 @@ size_t _strlen(char *str)
 	{
 		i++;
 	}
-	
+
 	return (i);
 }
 
 /**
- * append_text_to_file - appends a text of any file 
+ * append_text_to_file - appends a text of any file
  * at the end of the file.
  * @filename: name of the file that we will append
- * @text_content: NULL terminated 
+ * @text_content: NULL terminated
  * string to add at the end of the file
  *
  * Return: 1 on success and -1 on failure
@@ -35,7 +35,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 	fd = open(filename, O_WRONLY | O_APPEND);
-	
+
 	if (fd == -1)
 	{
 		return (-1);
@@ -44,7 +44,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	{
 		len = write(fd, text_content, _strlen(text_content));
 	}
-	
+
 	close(fd);
 	if (len == -1)
 	{
