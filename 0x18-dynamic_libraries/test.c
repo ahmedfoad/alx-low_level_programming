67a274aacf4a7169c6e37fea465ff65c1c9d06ca
AhmedFAly <eng.ahmedfoad@gmail.com>
2023-06-19 23:45:32 +0300

    export output

--- 0x18-dynamic_libraries/test.c
+++ 0x18-dynamic_libraries/test.c
@@ -1,1 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include <unistd.h>
+
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+int mul(int a, int b)
+{
+	return(a * b);
+}
+
+
+int mod(int a, int b)
+{
+	if (b != 0)
+		return (a % b);
+	return (0);
+}
 
