abf94f1382db58a94567436da2d49988dfc8638b
AhmedFAly <eng.ahmedfoad@gmail.com>
2023-06-23 11:59:26 +0300

    betty

--- 0x18-dynamic_libraries/atoi.c
+++ 0x18-dynamic_libraries/atoi.c
@@ -1,41 +1,45 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * _atoi - gets sign and numbers of string
- * @s: array
- * Return: gets numbers with its sign
+ * _atoi - Converts a string to an integer.
+ *
+ * @s: The string
+ * to convert.
+ *
+ * Return: The integer
+ * value of the string.
  */
 int _atoi(char *s)
 {
-	unsigned int cont1 = 0, a, b, c, num = 0, tam;
+	unsigned int counter = 0, var1, var2, var3, sum = 0, temp;
 	int aux2 = 1;
 
-	while (*(s + cont1) != '\0')
+	while (*(s + counter) != '\0')
 	{
-		cont1++;
+		counter++;
 	}
-	for (a = 0; a < cont1; a++)
+	for (var1 = 0; var1 < counter; var1++)
 	{
-		if (*(s + a) >= '0' && *(s + a) <= '9')
+		if (*(s + var1) >= '0' && *(s + var1) <= '9')
 			break;
 	}
-	for (b = a; b < cont1; b++)
+	for (var2 = var1; var2 < counter; var2++)
 	{
-		if (!(*(s + b) >= '0' && *(s + b) <= '9'))
+		if (!(*(s + var2) >= '0' && *(s + var2) <= '9'))
 			break;
 	}
-	for (c = 0; c < a; c++)
+	for (var3 = 0; var3 < var1; var3++)
 	{
-		if (*(s + c) == '-')
+		if (*(s + var3) == '-')
 			aux2 = aux2 * (-1);
 	}
-	tam = b - a;
-	while (tam >= 1)
+	temp = var2 - var1;
+	while (temp >= 1)
 	{
-		num = (num * 10) + (*(s + a) - '0');
-		a++;
-		tam--;
+		sum = (sum * 10) + (*(s + var1) - '0');
+		var1++;
+		temp--;
 	}
-	num = num * aux2;
-	return (num);
+	sum = sum * aux2;
+	return (sum);
 }
