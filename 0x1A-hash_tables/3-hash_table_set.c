63ded9e9ba56d262d1a8a454a7353b0eb7506a24
ahmedfoad <eng.ahmedfoad@gmail.com>
2023-07-08 01:31:03 +0200

    444

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,38 +1,6 @@
 #include "hash_tables.h"
 
 /**
- * make_hash_node - creates a new hash node
- * @key: key for the node
- * @value: for the node
- *
- * Return: the new node, or NULL on failure
- */
-hash_node_t *make_hash_node(const char *key, const char *value)
-{
-	hash_node_t *node;
-
-	node = malloc(sizeof(hash_node_t));
-	if (node == NULL)
-		return (NULL);
-	node->key = strdup(key);
-	if (node->key == NULL)
-	{
-		free(node);
-		return (NULL);
-	}
-	node->value = strdup(value);
-	if (node->value == NULL)
-	{
-		free(node->key);
-		free(node);
-		return (NULL);
-	}
-	node->next = NULL;
-	return (node);
-}
-
-
-/**
  * hash_table_set - sets a key to a value in the hash table
  * @ht: hash table to add elemt to
  * @key: key for the data
@@ -70,4 +38,36 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node->next = ht->array[index];
 	ht->array[index] = hash_node;
 	return (1);
+}
+
+
+/**
+ * make_hash_node - creates a new hash node
+ * @key: key for the node
+ * @value: for the node
+ *
+ * Return: the new node, or NULL on failure
+ */
+hash_node_t *make_hash_node(const char *key, const char *value)
+{
+	hash_node_t *node;
+
+	node = malloc(sizeof(hash_node_t));
+	if (node == NULL)
+		return (NULL);
+	node->key = strdup(key);
+	if (node->key == NULL)
+	{
+		free(node);
+		return (NULL);
+	}
+	node->value = strdup(value);
+	if (node->value == NULL)
+	{
+		free(node->key);
+		free(node);
+		return (NULL);
+	}
+	node->next = NULL;
+	return (node);
 }
